caaa027b6ad57d9d6ce703a8d5f2f2875495dafc
senixcode <codesenix@gmail.com>
2021-07-26 19:36:54 -0500

    feat: :tada: start setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: colors.indigo,
+      secondary: colors.blue,
+      neutral: colors.gray,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
