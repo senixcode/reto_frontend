f2beb2eb3161670773fe19b27d325a0445847d90
senixcode <codesenix@gmail.com>
2021-07-27 19:55:24 -0500

    feat: :sparkles: add localstorage welcome

--- src/context/WelcomeProvider.jsx
+++ src/context/WelcomeProvider.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 export const WelcomeContext = createContext()
+const STORAGE_WELCOME = 'welcome'
+
+const parseWelcomeLocalStorage = localStorage.getItem(STORAGE_WELCOME)
+  ? false
+  : true
 
 const WelcomeProvider = ({ children }) => {
-  const [welcome, setWelcome] = useState(true)
-
+  const [welcome, setWelcome] = useState(parseWelcomeLocalStorage)
+  useEffect(() => {
+    if (welcome === false) localStorage.setItem(STORAGE_WELCOME, 'true')
+  }, [welcome])
   return (
     <WelcomeContext.Provider value={{ welcome, setWelcome }}>
       {children}
