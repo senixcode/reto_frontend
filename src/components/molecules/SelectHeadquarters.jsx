dad959c00eb61d970b08a2dcca323db7deda066b
senixcode <codesenix@gmail.com>
2021-07-28 10:33:17 -0500

    feat: :sparkles: add error custom

--- src/components/molecules/SelectHeadquarters.jsx
+++ src/components/molecules/SelectHeadquarters.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 
-export default function SelectHeadquarters() {
+const messageDefault = "'* Es requerido este campo'"
+export default function SelectHeadquarters({
+  name,
+  register,
+  error,
+  customError = false,
+}) {
   return (
-    <div className="flex flex-col gap-y-1">
-      <label>Sede</label>
-      <select className="rounded-lg">
-        <option hidden value=""></option>
-        {['Miraflores', 'La Molina', 'San Isidro'].map((headquarter, i) => (
-          <option key={i} value={headquarter}>
-            {headquarter}
-          </option>
-        ))}
-      </select>
-    </div>
+    <>
+      <div className="flex flex-col gap-y-1">
+        <label> Sedes </label>
+        <select {...register} className="rounded-lg">
+          <option hidden value=""></option>
+          {['Miraflores', 'La Molina', 'San Isidro'].map((headquarter, i) => (
+            <option key={i} value={headquarter}>
+              {headquarter}
+            </option>
+          ))}
+        </select>
+      </div>
+      {error && (
+        <span className="text-red-600 text-sm">
+          {customError ? error.message || messageDefault : messageDefault}
+        </span>
+      )}
+    </>
   )
 }
