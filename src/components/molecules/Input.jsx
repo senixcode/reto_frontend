7bfbd7d71c25ebbf511aac1bfe651fc9ba4cbcdf
senixcode <codesenix@gmail.com>
2021-07-28 07:44:47 -0500

    refactor: :recycle: refactor for the implementation of react-hook-form

--- src/components/molecules/Input.jsx
+++ src/components/molecules/Input.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
-export default function Input({ name }) {
+const messageDefault = "'* Es requerido este campo'"
+export default function Input({
+  name,
+  type = 'text',
+  register,
+  error,
+  customError = false,
+}) {
   return (
-    <div className="flex flex-col gap-y-1">
-      <label>{name}</label>
-      <input type="text" className="rounded-lg" />
-    </div>
+    <>
+      <div className="flex flex-col gap-y-1">
+        <label>{name}</label>
+        <input type={type} {...register} className="rounded-lg" />
+      </div>
+      {error && (
+        <span className="text-red-600">
+          {customError ? error.message || messageDefault : messageDefault}
+        </span>
+      )}
+    </>
   )
 }
