aa361a5af86005e9d99b409cf9469ccc01a01565
senixcode <codesenix@gmail.com>
2021-07-28 15:48:53 -0500

    refactor: :recycle: clean Modal

--- src/components/molecules/Modal.jsx
+++ src/components/molecules/Modal.jsx
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react'
-import { Fragment, useState } from 'react'
+import { Fragment } from 'react'
 export default function Modal({ isOpen, closeModal, children }) {
   return (
     <Transition appear show={isOpen} as={Fragment}>
