0aedb941b5a7e9151659878fc29abb2fb1b9d8a2
senixcode <codesenix@gmail.com>
2021-07-28 10:28:16 -0500

    style: :lipstick: fix min-h-screen

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -3,9 +3,9 @@ import FormSignup from '../components/containers/FormSignup'
 
 export default function SignUp() {
   return (
-    <main className="flex flex-col md:flex-row h-screen items-center ">
+    <main className="flex flex-col md:flex-row min-h-screen items-center">
       <SigninImgSvg />
-      <div className="bg-white w-full md:w-1/2  h-screen px-6 lg:px-16 flex items-center justify-center">
+      <div className=" bg-white w-full md:w-1/2 px-6 md:px-12 flex items-center justify-center">
         <div className="w-full h-100 ">
           <div className="flex w-full justify-center">
             <FormSignup />
