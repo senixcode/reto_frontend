83138e894c116f401ea7fa378e053b8f8ba5f25e
senixcode <codesenix@gmail.com>
2021-07-28 21:39:40 -0500

    refactor: :recycle: add user context

--- src/components/containers/ModalDetailUser.jsx
+++ src/components/containers/ModalDetailUser.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../../context/UserProvider'
+import formatePhone from '../../help/formatePhone'
 import Modal from '../molecules/Modal'
 
 export default function ModalDetailUser({ closeModal, isOpen }) {
+  const { user } = useContext(UserContext)
+  const detail = [
+    {
+      icon: 'uil uil-file-alt',
+      name: user?.SpecializationProgram,
+    },
+    {
+      icon: 'uil uil-location-point',
+      name: user?.headquarters,
+    },
+    {
+      icon: 'uil uil-mobile-android',
+      name: formatePhone(user?.phone),
+    },
+    {
+      icon: 'uil uil-message',
+      name: user?.email,
+    },
+  ]
   return (
     <Modal closeModal={closeModal} isOpen={isOpen}>
       <div className="flex justify-between items-center pb-1 border-b-2 border-neutral-400">
@@ -11,36 +32,18 @@ export default function ModalDetailUser({ closeModal, isOpen }) {
         </span>
       </div>
       <div className="my-2">
-        <h3 className="font-semibold text-lg">Nombre del usuario</h3>
-        <p className="mt-1 text-neutral-600">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
-          accusantium debitis illo alias facilis pariatur deleniti iste eveniet
-          sunt aut enim, assumenda, fugit sit eligendi expedita perferendis sint
-          maxime distinctio.
-        </p>
+        <h3 className="font-semibold text-lg">{`${user?.names} ${user?.lastName}`}</h3>
+        <p className="mt-1 text-neutral-600"> {user?.profile}</p>
       </div>
       <div>
-        {[
-          {
-            icon: 'uil uil-file-alt',
-            name: 'Front-End',
-          },
-          {
-            icon: 'uil uil-location-point',
-            name: 'Front-End',
-          },
-          {
-            icon: 'uil uil-mobile-android',
-            name: '977 663 454',
-          },
-          {
-            icon: 'uil uil-message',
-            name: 'hola@gmail.com',
-          },
-        ].map(({ icon, name }, i) => (
+        {detail.map(({ icon, name }, i) => (
           <div key={i} className="flex gap-x-6 items-center">
-            <i className={`${icon} text-primary-900 text-lg`} />
-            <span>{name}</span>
+            {name && (
+              <>
+                <i className={`${icon} text-primary-900 text-lg`} />
+                <span>{name}</span>
+              </>
+            )}
           </div>
         ))}
         <button className="btn_primary">Mandar mensaje</button>
