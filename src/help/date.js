8a0ada9374df527f450c21a8bb2770f426b8eabd
senixcode <codesenix@gmail.com>
2021-07-28 21:54:21 -0500

    feat: :sparkles: add validation date

--- src/help/date.js
+++ src/help/date.js
@@ -1,10 +1,14 @@
 import { addHours, format } from 'date-fns'
 
 export const formDateToDayMonthYear = (formDate) => {
-  const date = new Date(formDate)
-  return format(addHours(date, 12), 'dd-MM-yyyy')
+  if (formDate) {
+    const date = new Date(formDate)
+    return format(addHours(date, 12), 'dd-MM-yyyy')
+  }
 }
 export const formDateToYearMonthDay = (formDate) => {
-  const date = new Date(formDate)
-  return format(addHours(date, 12), 'yyyy-MM-dd')
+  if (formDate) {
+    const date = new Date(formDate)
+    return format(addHours(date, 12), 'yyyy-MM-dd')
+  }
 }
